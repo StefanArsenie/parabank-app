47854449d2a5d4d2bb7f251dad471ee7334931ec
StefanArsenie <arseniestefan@gmail.com>
2026-07-17 16:52:39 +0300

    Add new user

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,6 +2,6 @@ import { test, expect} from '@fixtures/pageFixtures';
 
 test('user login in with valid credentials', async ({loginPage, page}) => {
     await loginPage.navigate();
-    await loginPage.login('fish', 'fish123');
+    await loginPage.login('pui', 'pui123');
     await expect(page).toHaveURL(/overview\.htm/);
 })
