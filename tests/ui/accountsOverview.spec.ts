df4cc28f2507381c5709729ae4c3087a6b2d62c6
StefanArsenie <arseniestefan@gmail.com>
2026-08-30 18:41:15 +0300

    new modfications

--- tests/ui/accountsOverview.spec.ts
+++ tests/ui/accountsOverview.spec.ts
@@ -1,19 +1,18 @@
 import {test, expect} from "@ui/fixtures/pageFixtures";
 
 test.describe('Accounts Overview Page', () => {
-    test('Display the account with valid balance', {tag: '@regression'}, async ({page, accountOverviewPage}) => {
+    test('Display the account with valid balance', {tag: '@regression'}, async ({accountOverviewPage}) => {
         await test.step('Verify that page is displayed', async() => {
             await expect(accountOverviewPage.title).toHaveText(' Accounts Overview ')
         })
         const account = await accountOverviewPage.getAccounts()
         await test.step('Verify that at least one account is displayed', async() => {
-            ;
-            expect(account.length).toBeGreaterThan(0);
+            await expect(account.length).toBeGreaterThan(0);
         })
 
         await test.step('Verify that balance has expected format', async () => {
             account.forEach(account => {
-                expect(account.balance).toMatch(/^-?\$\d+\.\d{2}$/);
+                await expect(account.balance).toMatch(/^-?\$\d+\.\d{2}$/);
             })
         })
     })
